f5c7bbcaf693bbb7474aae86a83200a606110051
mazzanek <158159078+mazzanek@users.noreply.github.com>
2024-07-02 19:42:02 +0200

    vytvoreni databvaze

--- forms_and_db/members/models.py
+++ forms_and_db/members/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+
+  def __str__(self):
+    return f'{self.lastname}, {self.firstname}'
